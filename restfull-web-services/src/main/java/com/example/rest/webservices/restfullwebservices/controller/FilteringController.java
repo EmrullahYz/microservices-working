a81b3d89d18a7eb89c8e4f3a9beeac1ee32535be
EmrullahYz <emrullahyilmzv2@gmail.com>
2022-11-06 23:50:48 +0300

    added Dynamic Filtering example

--- restfull-web-services/src/main/java/com/example/rest/webservices/restfullwebservices/controller/FilteringController.java
+++ restfull-web-services/src/main/java/com/example/rest/webservices/restfullwebservices/controller/FilteringController.java
@@ -1,13 +1,19 @@
 package com.example.rest.webservices.restfullwebservices.controller;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,6 +27,16 @@ class SomeBean{
     private String field2;
     private String field3;
 }
+
+@Getter
+@Setter
+@AllArgsConstructor
+@JsonFilter("SomeBean2Filter")
+class SomeBean2{
+    private String field1;
+    private String field2;
+    private String field3;
+}
 @RestController
 public class FilteringController {
 
@@ -31,6 +47,28 @@ public class FilteringController {
     @GetMapping("/filteringList")
     public List<SomeBean> filteringList(){
         return Arrays.asList(new SomeBean("value1","value2","value3"),new SomeBean("value4","value5","value6"));
+    }
+
+    @GetMapping("/filtering2")
+    public MappingJacksonValue filtering2(){
+        SomeBean2 bean2 = new SomeBean2("value1","value2","value3");
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(bean2);
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1","field3");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBean2Filter", filter);
+
+        mappingJacksonValue.setFilters(filters);
+        return mappingJacksonValue;
+    }
+
+    @GetMapping("/filtering2List")
+    public MappingJacksonValue filtering2List(){
+        List<SomeBean2> list = Arrays.asList(new SomeBean2("value1","value2","value3"),new SomeBean2("value4","value5","value6"));
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(list);
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field2","field3");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBean2Filter", filter);
+
+        mappingJacksonValue.setFilters(filters);
+        return mappingJacksonValue;
     }
 
 }
